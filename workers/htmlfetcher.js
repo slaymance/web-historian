041902aa418bece5a65cf698fdfa9e9c1281f0b3
Shane Laymance <shane.laymance@gmail.com>
2017-09-16 15:29:03 -0700

    Implement worker process sites.txt functionality, fetching, and writing to correct directory structure

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -8,20 +8,23 @@ var archives = require('../helpers/archive-helpers.js');
 var finished = false;
 var written = false;
 
-var fetchSite = function(url, dirPath) {
-  https.get(url, response => {
+var fetchSite = function(url, dirPath, list, index) {
+  console.log(`https://${url}`);
+  https.get(`https://${url}`, response => {
     response.setEncoding('utf8');
     var body = '';
     response.on('data', chunk => {
       body += chunk;
     });
     response.on('end', () => {
-      var pathname = path.join(dirPath, 'index.html'); // FIX ME?
+      var pathname = path.join(dirPath, '/index.html'); // FIX ME?
       fs.writeFile(pathname, body, err => {
         if (!err) {
-          written = true;
+          list.splice(index, 1);
+          if (index = list.length - 1) {
+            fs.writeFile(path.join(__dirname, '../archives/sites.txt'), list);
+          }
         }
-        finished = true;
       });
     });
   });
@@ -32,21 +35,14 @@ var processList = function(url, array) {
   var names = array.map((name) => {
     return archives.translate(name);
   });
-  var dirPath = path.join(__dirname, '../archives/', url);
+  var dirPath = path.join(__dirname, '../archives/');
 
   for (var i = 0; i < names.length; i++) {
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath);
+    if (!fs.existsSync(dirPath + names[i])) {
+      fs.mkdirSync(dirPath + names[i]);
     }
-    fetchSite(url, dirPath);
-    while (!finished) {
-      console.log('we love asynchronicity!!');
-    }
-    if (written) {
-      toDoList.splice(i, 1);
-    }
+    fetchSite(names[i], path.join(dirPath, names[i]), toDoList, i);
   }
-  fs.writeFile(path.join(__dirname, '../archives/sites.txt'));
 };
 
 // check the dl list
